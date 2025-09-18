ea7d605b49ef8ad452ce0c276471d78c31fbf946
MarianaBacetto <162622849+MarianaBacetto@users.noreply.github.com>
2025-09-18 18:15:45 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-function minhafuncao (){
-    document.write(5+5)
+document.getElementById("texto").innerHTML = "Meu primeiro texto <b>JS</b>"
+console.log("Oi isso é um console.log");
+
+
+let a, b, c
+a = 5;
+b = 6;
+c = 7;
+
+
+let pessoa = "Dimitri" ;
+let pessoa="Dimitri";
+
+
+document
+    .getElementById("texto").innerHTML =
+    "Meu primeiro texto <b>JS</b>"
+
+
+function minhafuncao() {
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
 }
